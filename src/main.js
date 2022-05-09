bedb3f0efd2f48d6fa865c43c933907fb29d81fc
tsegaw-alt <grace.d224@yahoo.com>
2022-05-09 17:18:01 +0300

    icons added

--- src/main.js
+++ src/main.js
@@ -4,11 +4,12 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/'
 import './scss/global.scss'
+import { IconsPlugin } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
-
+Vue.use(IconsPlugin)
 new Vue({
   router,
   store,
